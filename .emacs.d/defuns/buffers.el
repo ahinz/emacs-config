145911cd4c216e02e043ea28ac909801f02311c7
Adam Hinz <hinz.adam@gmail.com>
2013-01-31 23:55:54 -0500

    Update some buffer functions

---
+++ .emacs.d/defuns/buffers.el
@@ -0,0 +1,18 @@
+(defun ah/cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a before-save-hook, and that
+might be bad."
+  (interactive)
+  (untabify-buffer)
+  (delete-trailing-whitespace)
+  (set-buffer-file-coding-system 'utf-8))
+
+;; When popping the mark, continue popping until the cursor actually moves
+(defadvice pop-to-mark-command (around ensure-new-position activate)
+  (let ((p (point)))
+    (dotimes (i 10)
+      (when (= p (point)) ad-do-it))))
+
+
+;; Various superfluous white-space. Just say no.
+(add-hook 'before-save-hook 'ah/cleanup-buffer-safe)
