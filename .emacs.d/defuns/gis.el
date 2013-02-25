be22d37880e82019f517edfba7d288a82831de77
Adam Hinz <hinz.adam@gmail.com>
2013-02-25 16:23:09 -0500

    Cleanup whitespace

--- .emacs.d/defuns/gis.el
+++ .emacs.d/defuns/gis.el
@@ -1,8 +1,8 @@
 (defun to-webm (lat lng)
   (let ((origin-shift (/ (* 2.0 pi (/ 6378137.0 2.0)) 180.0)))
-    (list 
+    (list
      (* origin-shift lng)
-     (* origin-shift 
+     (* origin-shift
         (/ (log (tan (*
                       (+ 90.0 lat)
                       (/ pi 360.0))))
@@ -13,7 +13,7 @@
         (d2r (/ pi 180.0))
         (r2d (/ 180.0 pi)))
     (list
-     (* r2d (- 
+     (* r2d (-
              (* 2.0 (atan (exp (* d2r (/ y origin-shift)))))
              (/ pi 2.0)))
      (/ x origin-shift))))
@@ -27,7 +27,7 @@
 
 (defun bbox-to-wkt (xmin ymin xmax ymax)
   (format "POLYGON ((%s))"
-          (mapconcat 'identity 
+          (mapconcat 'identity
                      (mapcar (lambda (z) (format "%f,%f" (first z) (second z)))
                              (bbox-to-polylist xmin ymin xmax ymax))
                      " ")))
