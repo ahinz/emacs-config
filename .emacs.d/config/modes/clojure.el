3e35e5bced522a136d1419eeed941e42a3378445
Adam Hinz <hinz.adam@gmail.com>
2013-03-25 15:26:19 -0400

    Sane stack trace settings

--- .emacs.d/config/modes/clojure.el
+++ .emacs.d/config/modes/clojure.el
@@ -1,6 +1,9 @@
 (ah/prepend-plugin-to-load-path "clojure-mode")
 
 (require 'clojure-mode)
+(require 'clojure-test-mode)
+
+(setq nrepl-popup-stacktraces nil)
 
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
